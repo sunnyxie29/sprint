9ae98fe84e7a5b104531944c95f1f0ce8de80acb
Sunny <sunnyxie29@gmail.com>
2026-09-22 15:32:00 -0400

    added one more route

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to our dining hall tracker.");
 });
 
+app.get("/menu", (req, res) => {
+  res.send("Today's Dining Hall Menu");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
